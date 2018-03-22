eeca9cd64338992a1ecad0fc50bd267c56a39256
michael hohl <mhohl@rocketmail.com>
2018-03-22 18:07:53 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artists_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count =+ 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def count
+    @@count
+  end
+
+  def genres
+    @@genres.uniq
+  end
+
+end
